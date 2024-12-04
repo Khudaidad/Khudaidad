79b2f504cd42e20f0e81d34d56539a0ac58d025f
Khudaidad <khudaded0@gmail.com>
2024-12-05 01:32:56 +0330

    Update eyeddictapp.py

--- dictapp.py
+++ dictapp.py
@@ -2,21 +2,23 @@ import streamlit as st
 
 # Sample dictionary data
 dictionary = {
-    "apple": "سیب.",
+    "apple": "You are the apple of my eye!.",
     "banana": "موز یا کیله.",
     "orange": "نارنج.",
-    "grape": "انگور."
+    "grape": "انگور.",
+    "watermelon": "هندوانه.",
+    "peach": "هلو.",
+    "pear": "گلابی."
 }
 
 # Title of the app
-st.title("Khudai_dad__'S Online Dictionary")
+st.title("Khudai_Dad__'s Online Dictionary")
 
 # Input field for user to enter a word
-word = st.text_input("                        لغت مورد نظر خود را وارد کنید :")
+word = st.text_input("لطفا لغت مورد نظر خود را وارد کنید:")
 
 # Check if the word is in the dictionary
 if word:
-    definition = dictionary.get(word.lower(), "Word not found in the dictionary.")
-    st.write(f"Definition: {definition}")
-    
-    
+    # Normalize the input by converting it to lowercase
+    definition = dictionary.get(word.lower(), "این لغت در دیکشنری موجود نیست.")
+    st.write(f"تعریف: {definition}")
